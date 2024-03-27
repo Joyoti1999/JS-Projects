1a59169c6f9faaaae699bf1030e1a74893953ca6
Joyoti1999 <139268254+Joyoti1999@users.noreply.github.com>
2024-03-27 16:21:48 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as a newer version
+
+// **** primitive data types ******//
+//alert('hello'); we are using node js not browser
+// number => 2 ... 53
+// bigint 
+// string  => ""
+// boolean =>
+// null => standalone value/ empty
+// undefind => not defind 
+// symbol => unique
+
+// object 
+
+console.log(typeof null); 
---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 1999
+let accountEmail = "joyoti3@gmail.com"
+var accountPassword = "12345"
+accountcity = "kolkata"
+// accountId = 1
+accountEmail = "test@gmail.com"
+accountPassword ="2345"
+accountcity ="mp"
+let accountstate;
+// console.log(accountPassword);
+console.table([accountEmail, accountPassword , accountcity , accountId, accountstate]);
